d14a94aba1e713a118da9a94e315ff2389f0d8c5
Ricardo Branco <rbranco@suse.de>
2019-04-18 21:57:03 +0200

    nginx: Support HTTP Basic Authentication

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 umask 077
 
+envsubst '$APP_PORT $NGINX_HOST' < /etc/nginx/conf.d/site.conf.template > /run/site.conf
+
 openssl_config() {
 	cat /etc/ssl/openssl.cnf
 	cat <<- EOF
@@ -26,8 +28,24 @@ if [ ! -f $SSL/key.pem ] ; then
 	openssl rand -base64 48 > $SSL/key.txt
 	# Generate a self-signed certificate
 	openssl req -x509 -sha512 -newkey rsa:4096 -keyout $SSL/key.pem -out $SSL/cert.pem -days 365 -subj "/CN=${NGINX_HOST:-localhost}" -passout file:$SSL/key.txt -extensions custom -config <(openssl_config)
-fi
 
-envsubst '$APP_PORT $NGINX_HOST' < /etc/nginx/conf.d/site.conf.template > /run/site.conf
+	# Generate a random salt of 16 characters
+	SALT=$(openssl rand -base64 12)
+	# The salt character set is [a-zA-Z0-9./]
+	SALT=${SALT//+/.}
+	if [ -z "$PASS" ] ; then
+		# Generate a random password
+		PASS=$(openssl rand -base64 48)
+		echo "Password for HTTP Basic Authentication is $PASS"
+	fi
+	# Hash password with 100000 rounds of salted SHA-512
+	#PASS=$(mkpasswd -m sha-512 -S "$SALT" -R 100000 "$PASS")
+	PASS=$(perl -e "print crypt('$PASS', '\$6\$rounds=100000\$$PASS')")
+	echo "test:$PASS" >> $SSL/auth.htpasswd
+	unset PASS
+	chmod 644 $SSL/auth.htpasswd
+elif [ ! -f $SSL/auth.htpasswd ] ; then
+	sed -i '/auth_basic/d' /run/site.conf
+fi
 
 exec nginx -g 'daemon off;'
