8febe97a1ebee9f50b4e71a955c4f32e6bbfa61b
Ricardo Branco <rbranco@suse.de>
2019-06-04 14:48:43 +0200

    s/klass/cls/g

--- cloudview/singleton.py
+++ cloudview/singleton.py
@@ -8,11 +8,11 @@ class Singleton:  # pylint: disable=too-few-public-methods
     Singleton decorator
     """
 
-    def __init__(self, klass):
-        self.klass = klass
+    def __init__(self, cls):
+        self.cls = cls
         self.instance = None
 
     def __call__(self, *args, **kwargs):
         if self.instance is None:
-            self.instance = self.klass(*args, **kwargs)
+            self.instance = self.cls(*args, **kwargs)
         return self.instance
