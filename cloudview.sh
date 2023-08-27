3d158625aff07a4e41315d837c6a653dcce94a52
Ricardo Branco <rbranco@suse.de>
2023-08-27 10:51:40 +0200

    Fix cloudview.sh

--- cloudview.sh
+++ cloudview.sh
@@ -1,65 +1,18 @@
 #!/bin/bash
 
+# Allow use of DOCKER=podman
+DOCKER="${DOCKER:-docker}"
 IMAGE="${IMAGE:-ghcr.io/ricardobranco777/cloudview:latest}"
 ARGS=("$@")
 
-check_certificates=(
+CHECK_CERTIFICATES=(
 	/etc/ssl/certs/ca-certificates.crt
 	/etc/pki/tls/certs/ca-bundle.crt
 	/etc/ssl/ca-bundle.pem
 	/etc/ssl/cert.pem
 )
 
-if [ -z "$REQUESTS_CA_BUNDLE" ] ; then
-	for file in "${check_certificates[@]}" ; do
-		if [ -f "$file" ] ; then
-			export REQUESTS_CA_BUNDLE="$file"
-		fi
-	done
-fi
-
-# Get -c option
-get_config () {
-	args=("$@")
-
-	index=-1
-	for i in "${!args[@]}"; do
-		if [[ ${args[i]} =~ ^-c|--config$ ]] ; then
-			index="$i"
-			break
-		fi
-	done
-	if [[ $index -ge 0 ]] ; then
-		echo "${args[$((++i))]}"
-	fi
-}
-
-# Add "-c ~/clouds.yaml" if it exists
-clouds_yaml="$(get_config "$@")"
-if [[ -z $clouds_yaml ]] ; then
-	clouds_yaml="${clouds_yaml:-$HOME/clouds.yaml}"
-	if [ -f "$clouds_yaml" ] ; then
-		ARGS+=(--config "$clouds_yaml")
-	fi
-fi
-
-# Mount as volumes all variables in clouds.yaml
-volumes=()
-if [ -f "$clouds_yaml" ] ; then
-	volumes+=(-v "$clouds_yaml:$clouds_yaml:ro")
-	mapfile -t files < <(grep -v ' *#' < "$clouds_yaml" | awk '$NF ~ /^\// { print $NF }' | sed -e 's/"\(.*\)"/\1/' -e "s/'\(.*\)'/\1/")
-	for file in "${files[@]}" ; do
-		if [ -f "$file" ] ; then
-			volumes+=(-v "$file:$file:ro")
-		fi
-	done
-fi
-
-# Add variables
-
-# Openstack
-mapfile -t check_variables < <(env | grep ^OS_ | awk -F= '{ print $1 }')
-check_variables+=(
+CHECK_VARIABLES=(
 	# System certificates
 	REQUESTS_CA_BUNDLE
 	# EC2
@@ -78,9 +31,74 @@ check_variables+=(
 	# GCE
 	GOOGLE_APPLICATION_CREDENTIALS
 )
+# Openstack
+mapfile -t openstack_variables < <(env | grep ^OS_ | awk -F= '{ print $1 }')
+CHECK_VARIABLES+=("${openstack_variables[@]}")
 
+name="cloudview$$"
+container_options=(
+	--rm		# Remove container after running
+	-it		# Allow user to interrupt execution
+	--net=host	# Use host's DNS
+	--name "$name"
+	-e LIBCLOUD_DEBUG="$LIBCLOUD_DEBUG"
+)
+
+check_certificates() {
+	if [ -z "$REQUESTS_CA_BUNDLE" ] ; then
+		for file in "${CHECK_CERTIFICATES[@]}" ; do
+			if [ -f "$file" ] ; then
+				export REQUESTS_CA_BUNDLE="$file"
+			fi
+		done
+	fi
+}
+
+check_certificates
+
+# Get clouds.yaml
+get_config () {
+	index=-1
+	for ((i=0; i<${#ARGS[@]}; i++)) ; do
+		if [[ ${ARGS[i]} =~ ^-c|--config$ ]] ; then
+			index="$i"
+			break
+		fi
+	done
+	if [[ $index -ge 0 ]] ; then
+		clouds_yaml="${ARGS[$((index+1))]}"
+		if [ -z "$clouds_yaml" ] ; then
+			echo "ERROR: the ${ARGS[$index]} option needs an argument" >&2
+			exit 1
+		elif [ ! -f "$clouds_yaml" ] ; then
+			echo "ERROR: No such file: $clouds_yaml" >&2
+			exit 1
+		fi
+	else
+		clouds_yaml="${clouds_yaml:-$HOME/clouds.yaml}"
+		if [ -f "$clouds_yaml" ] ; then
+			ARGS+=(--config "$clouds_yaml")
+		fi
+	fi
+}
+
+get_config
+
+# Mount as volumes all values in clouds.yaml that are pathnames
+volumes=()
+if [ -f "$clouds_yaml" ] ; then
+	volumes+=(-v "$clouds_yaml:$clouds_yaml:ro")
+	mapfile -t values < <(sed -re 's/#.*//' -e 's/"(.*)"/\1/' -e "s/'(.*)'/\1/" < "$clouds_yaml" | awk '$NF ~ /^\// { print $NF }')
+	for value in "${values[@]}" ; do
+		if [ -f "$value" ] ; then
+			volumes+=(-v "$value:$value:ro")
+		fi
+	done
+fi
+
+# Add variables
 variables=()
-for var in "${check_variables[@]}" ; do
+for var in "${CHECK_VARIABLES[@]}" ; do
 	if [[ -v $var ]] ; then
 		variables+=(-e "$var")
 	fi
@@ -90,6 +108,5 @@ for var in "${check_variables[@]}" ; do
 	fi
 done
 
-name="cloudview$$"
-echo docker run --rm -it --net=host --name "$name" -e LIBCLOUD_DEBUG="$LIBCLOUD_DEBUG" "${variables[@]}" "${volumes[@]}" "$IMAGE" "${ARGS[@]}"
-exec docker run --rm -it --net=host --name "$name" -e LIBCLOUD_DEBUG="$LIBCLOUD_DEBUG" "${variables[@]}" "${volumes[@]}" "$IMAGE" "${ARGS[@]}"
+echo docker run "${container_options[@]}" "${variables[@]}" "${volumes[@]}" "$IMAGE" "${ARGS[@]}"
+exec docker run "${container_options[@]}" "${variables[@]}" "${volumes[@]}" "$IMAGE" "${ARGS[@]}"
