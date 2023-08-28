86e87681bfbc2c39d875e49a18113b319eed4a72
Ricardo Branco <rbranco@suse.de>
2023-08-28 10:26:19 +0200

    Add unit tests for CSP

--- tests/test_instance.py
+++ tests/test_instance.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,missing-class-docstring,no-member
 import pytest
-from cloudview.instance import Instance
+from cloudview.instance import Instance, CSP
 
 
 def test_instance_creation():
@@ -32,3 +32,43 @@ def test_instance_unknown_attribute():
     instance = Instance()
     with pytest.raises(AttributeError):
         _ = instance["unknown_attribute"]
+
+
+class MockCSP(CSP):
+    def _get_instances(self):
+        return [
+            Instance(identifier="id1", name="Instance1", extra={"status": "running"}),
+            Instance(identifier="id2", name="Instance2", extra={"status": "stopped"}),
+        ]
+
+    def _get_instance(self, identifier, params):
+        if identifier == "id1":
+            return Instance(
+                identifier="id1", name="Instance1", extra={"name=": "Instance1"}
+            )
+        return None
+
+
+def test_csp_creation():
+    csp = MockCSP(cloud="MyCloud")
+    assert csp.cloud == "MyCloud"
+
+
+def test_csp_get_instances():
+    csp = MockCSP()
+    instances = csp.get_instances()
+    assert len(instances) == 2
+    assert instances[0].name == "Instance1"
+    assert instances[1].identifier == "id2"
+
+
+def test_csp_get_instance():
+    csp = MockCSP()
+    instance = csp.get_instance("id1")
+    assert instance["status"] == "running"
+
+
+def test_csp_get_instance_not_found():
+    csp = MockCSP()
+    instance = csp.get_instance("unknown_id")
+    assert instance is None
